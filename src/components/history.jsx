bc94b53280a3700615e43c073ca8898dc178dccc
Om Bhatnagar <omishb2001@gmail.com>
2023-03-26 14:54:23 +0530

    Toggling USD to EUR now uses result field as input.

--- src/components/history.jsx
+++ src/components/history.jsx
@@ -1,14 +1,7 @@
 import HistoryTable from "./shared/historytable";
 
 const History = ({ historyData }) => {
-	const tableHeaders = [
-		"No.",
-		"Fx",
-		"Override",
-		"USD to EUR",
-		"Initial Amount",
-		"Converted Amount",
-	];
+	const tableHeaders = ["No.", "Fx", "Override", "USD to EUR", "EUR", "USD"];
 	const trimmedData = historyData.slice(0, 5);
 	return (
 		<div className='w-5/6 mx-auto'>
